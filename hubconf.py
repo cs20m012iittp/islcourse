8ba90df062e252d4ffdc8599e30448ac76fb21ce
cs20m012iittp <116139036+cs20m012iittp@users.noreply.github.com>
2022-10-19 11:12:30 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,1 +1,137 @@
 
+import torch
+from torch import nn
+
+def kali():
+  print ('kali')
+  
+# Define a neural network YOUR ROLL NUMBER (all small letters) should prefix the classname
+class cs20m012(nn.Module):
+  def __init__(self, numChannels, classes):
+    super(LeNet, self).__init__()
+    # initialize first set of CONV => RELU => POOL layers
+    self.conv1 = Conv2d(in_channels=numChannels, out_channels=20,
+      kernel_size=(5, 5))
+    self.relu1 = ReLU()
+    self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+    # initialize second set of CONV => RELU => POOL layers
+    self.conv2 = Conv2d(in_channels=20, out_channels=50,
+      kernel_size=(5, 5))
+    self.relu2 = ReLU()
+    self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+    # initialize first (and only) set of FC => RELU layers
+    self.fc1 = Linear(in_features=800, out_features=500)
+    self.relu3 = ReLU()
+    # initialize our softmax classifier
+    self.fc2 = Linear(in_features=500, out_features=classes)
+    self.logSoftmax = LogSoftmax(dim=1)
+
+    
+  def forward(self, x):
+    # pass the input through our first set of CONV => RELU =>
+    # POOL layers
+    x = self.conv1(x)
+    x = self.relu1(x)
+    x = self.maxpool1(x)
+    # pass the output from the previous layer through the second
+    # set of CONV => RELU => POOL layers
+    x = self.conv2(x)
+    x = self.relu2(x)
+    x = self.maxpool2(x)
+    # flatten the output from the previous layer and pass it
+    # through our only set of FC => RELU layers
+    x = flatten(x, 1)
+    x = self.fc1(x)
+    x = self.relu3(x)
+    # pass the output to our softmax classifier to get our output
+    # predictions
+    x = self.fc2(x)
+    output = self.logSoftmax(x)
+    # return the output predictions
+    return output
+    
+# sample invocation torch.hub.load(myrepo,'get_model',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
+def get_model(train_data_loader=None, n_epochs=5):
+  model = None
+  INIT_LR = 1e-3
+  BATCH_SIZE = 64
+  EPOCHS = 5
+  # define the train and val splits
+  TRAIN_SPLIT = 0.75
+  VAL_SPLIT = 1 - TRAIN_SPLIT
+  # set the device we will be using to train the model
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  print("[INFO] loading the KMNIST dataset...")
+  trainData = KMNIST(root="data", train=True, download=True,
+	transform=ToTensor())
+  testData = KMNIST(root="data", train=False, download=True,
+	transform=ToTensor())
+  # calculate the train/validation split
+  print("[INFO] generating the train/validation split...")
+  numTrainSamples = int(len(trainData) * TRAIN_SPLIT)
+  numValSamples = int(len(trainData) * VAL_SPLIT)
+  (trainData, valData) = random_split(trainData,
+	[numTrainSamples, numValSamples],
+	generator=torch.Generator().manual_seed(42))
+  
+  # initialize the train, validation, and test data loaders
+  trainDataLoader = DataLoader(trainData, shuffle=True,
+	batch_size=BATCH_SIZE)
+  valDataLoader = DataLoader(valData, batch_size=BATCH_SIZE)
+  testDataLoader = DataLoader(testData, batch_size=BATCH_SIZE)
+  # calculate steps per epoch for training and validation set
+  trainSteps = len(trainDataLoader.dataset) // BATCH_SIZE
+  valSteps = len(valDataLoader.dataset) // BATCH_SIZE
+
+  # write your code here as per instructions
+  # ... your code ...
+  # ... your code ...
+  # ... and so on ...
+  # Use softmax and cross entropy loss functions
+  # set model variable to proper object, make use of train_data
+  
+  #print ('Returning model... (rollnumber: )')
+  
+  return model
+
+# sample invocation torch.hub.load(myrepo,'get_model_advanced',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
+def get_model_advanced(train_data_loader=None, n_epochs=10,lr=1e-4,config=None):
+  model = None
+  
+
+  # write your code here as per instructions
+  # ... your code ...
+  # ... your code ...
+  # ... and so on ...
+  # Use softmax and cross entropy loss functions
+  # set model variable to proper object, make use of train_data
+  
+  # In addition,
+  # Refer to config dict, where learning rate is given, 
+  # List of (in_channels, out_channels, kernel_size, stride=1, padding='same')  are specified
+  # Example, config = [(1,10,(3,3),1,'same'), (10,3,(5,5),1,'same'), (3,1,(7,7),1,'same')], it can have any number of elements
+  # You need to create 2d convoution layers as per specification above in each element
+  # You need to add a proper fully connected layer as the last layer
+  
+  # HINT: You can print sizes of tensors to get an idea of the size of the fc layer required
+  # HINT: Flatten function can also be used if required
+  return model
+  
+  
+  print ('Returning model... (rollnumber: xx)')
+  
+
+
+# sample invocation torch.hub.load(myrepo,'test_model',model1=model,test_data_loader=test_data_loader,force_reload=True)
+def test_model(model1=None, test_data_loader=None):
+
+  accuracy_val, precision_val, recall_val, f1score_val = 0, 0, 0, 0
+  # write your code here as per instructions
+  # ... your code ...
+  # ... your code ...
+  # ... and so on ...
+  # calculate accuracy, precision, recall and f1score
+  
+  print ('Returning metrics... (rollnumber: xx)')
+  
+  return accuracy_val, precision_val, recall_val, f1score_val
