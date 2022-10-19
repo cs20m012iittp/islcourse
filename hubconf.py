29144199a49f918dd8aaf17c4f48ac1a0177e9bc
cs20m012iittp <116139036+cs20m012iittp@users.noreply.github.com>
2022-10-19 11:31:40 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -82,25 +82,94 @@ def get_model(train_data_loader=None, n_epochs=5):
   # calculate steps per epoch for training and validation set
   trainSteps = len(trainDataLoader.dataset) // BATCH_SIZE
   valSteps = len(valDataLoader.dataset) // BATCH_SIZE
-
-
-print("[INFO] initializing the LeNet model...")
-model = LeNet(
+  print("[INFO] initializing the LeNet model...")
+  model = cs20m012(
 	numChannels=1,
 	classes=len(trainData.dataset.classes)).to(device)
-# initialize our optimizer and loss function
-opt = Adam(model.parameters(), lr=INIT_LR)
-lossFn = nn.NLLLoss()
-# initialize a dictionary to store training history
-H = {
+  # initialize our optimizer and loss function
+  opt = Adam(model.parameters(), lr=INIT_LR)
+  lossFn = nn.NLLLoss()
+  # initialize a dictionary to store training history
+  H = {
 	"train_loss": [],
 	"train_acc": [],
 	"val_loss": [],
 	"val_acc": []
-}
-# measure how long training is going to take
-print("[INFO] training the network...")
-startTime = time.time()
+  }
+  # measure how long training is going to take
+  print("[INFO] training the network...")
+  startTime = time.time()
+
+
+ 
+  # loop over our epochs
+  for e in range(0, EPOCHS):
+    # set the model in training mode
+    model.train()
+    # initialize the total training and validation loss
+    totalTrainLoss = 0
+    totalValLoss = 0
+    # initialize the number of correct predictions in the training
+    # and validation step
+    trainCorrect = 0
+    valCorrect = 0
+    # loop over the training set
+    for (x, y) in trainDataLoader:
+    # send the input to the device
+      (x, y) = (x.to(device), y.to(device))
+    # perform a forward pass and calculate the training loss
+      pred = model(x)
+      loss = lossFn(pred, y)
+    # zero out the gradients, perform the backpropagation step,
+    # and update the weights
+      opt.zero_grad()
+      loss.backward()
+      opt.step()
+    # add the loss to the total training loss so far and
+    # calculate the number of correct predictions
+      totalTrainLoss += loss
+      trainCorrect += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+
+    with torch.no_grad():
+    # set the model in evaluation mode
+      model.eval()
+    # loop over the validation set
+      for (x, y) in valDataLoader:
+      # send the input to the device
+        (x, y) = (x.to(device), y.to(device))
+      # make the predictions and calculate the validation loss
+        pred = model(x)
+        totalValLoss += lossFn(pred, y)
+      # calculate the number of correct predictions
+        valCorrect += (pred.argmax(1) == y).type(torch.float).sum().item()
+    avgTrainLoss = totalTrainLoss / trainSteps
+    avgValLoss = totalValLoss / valSteps
+  # calculate the training and validation accuracy
+    trainCorrect = trainCorrect / len(trainDataLoader.dataset)
+    valCorrect = valCorrect / len(valDataLoader.dataset)
+  # update our training history
+    H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
+    H["train_acc"].append(trainCorrect)
+    H["val_loss"].append(avgValLoss.cpu().detach().numpy())
+    H["val_acc"].append(valCorrect)
+  # print the model training and validation information
+    print("[INFO] EPOCH: {}/{}".format(e + 1, EPOCHS))
+    print("Train loss: {:.6f}, Train accuracy: {:.4f}".format(avgTrainLoss, trainCorrect))
+    print("Val loss: {:.6f}, Val accuracy: {:.4f}\n".format(avgValLoss, valCorrect))
+  # we can now evaluate the network on the test set
+    print("[INFO] evaluating network...")
+
+
+ 
+  
+
+
+  
+ 
+#plt.savefig(args["plot"])
+# serialize the model to disk
+#torch.save(model, args["model"])
 
   # write your code here as per instructions
   # ... your code ...
@@ -145,6 +214,120 @@ def get_model_advanced(train_data_loader=None, n_epochs=10,lr=1e-4,config=None):
 def test_model(model1=None, test_data_loader=None):
 
   accuracy_val, precision_val, recall_val, f1score_val = 0, 0, 0, 0
+
+
+  %matplotlib inline
+# loop over our epochs
+for e in range(0, EPOCHS):
+  # set the model in training mode
+  model.train()
+  # initialize the total training and validation loss
+  totalTrainLoss = 0
+  totalValLoss = 0
+  # initialize the number of correct predictions in the training
+  # and validation step
+  trainCorrect = 0
+  valCorrect = 0
+  # loop over the training set
+  for (x, y) in trainDataLoader:
+    # send the input to the device
+    (x, y) = (x.to(device), y.to(device))
+    # perform a forward pass and calculate the training loss
+    pred = model(x)
+    loss = lossFn(pred, y)
+    # zero out the gradients, perform the backpropagation step,
+    # and update the weights
+    opt.zero_grad()
+    loss.backward()
+    opt.step()
+    # add the loss to the total training loss so far and
+    # calculate the number of correct predictions
+    totalTrainLoss += loss
+    trainCorrect += (pred.argmax(1) == y).type(
+      torch.float).sum().item()
+
+
+  with torch.no_grad():
+    # set the model in evaluation mode
+    model.eval()
+    # loop over the validation set
+    for (x, y) in valDataLoader:
+      # send the input to the device
+      (x, y) = (x.to(device), y.to(device))
+      # make the predictions and calculate the validation loss
+      pred = model(x)
+      totalValLoss += lossFn(pred, y)
+      # calculate the number of correct predictions
+      valCorrect += (pred.argmax(1) == y).type(torch.float).sum().item()
+    avgTrainLoss = totalTrainLoss / trainSteps
+    avgValLoss = totalValLoss / valSteps
+  # calculate the training and validation accuracy
+    trainCorrect = trainCorrect / len(trainDataLoader.dataset)
+    valCorrect = valCorrect / len(valDataLoader.dataset)
+  # update our training history
+    H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
+    H["train_acc"].append(trainCorrect)
+    H["val_loss"].append(avgValLoss.cpu().detach().numpy())
+    H["val_acc"].append(valCorrect)
+  # print the model training and validation information
+  print("[INFO] EPOCH: {}/{}".format(e + 1, EPOCHS))
+  print("Train loss: {:.6f}, Train accuracy: {:.4f}".format(
+    avgTrainLoss, trainCorrect))
+  print("Val loss: {:.6f}, Val accuracy: {:.4f}\n".format(
+    avgValLoss, valCorrect))
+  endTime = time.time()
+  print("[INFO] total time taken to train the model: {:.2f}s".format(
+  endTime - startTime))
+  # we can now evaluate the network on the test set
+  print("[INFO] evaluating network...")
+
+
+  # turn off autograd for testing evaluation
+  with torch.no_grad():
+    # set the model in evaluation mode
+    model.eval()
+
+    # initialize a list to store our predictions
+    preds = []
+    # loop over the test set
+    for (x, y) in testDataLoader:
+      # send the input to the device
+      x = x.to(device)
+      # make the predictions and add them to the list
+      pred = model(x)
+      preds.extend(pred.argmax(axis=1).cpu().numpy())
+  # generate a classification report
+    print(classification_report(testData.targets.cpu().numpy(),
+    np.array(preds), target_names=testData.classes))
+
+
+  
+  # plot the training loss and accuracy
+  plt.style.use("ggplot")
+  plt.figure()
+  plt.plot(H["train_loss"], label="train_loss")
+  plt.plot(H["val_loss"], label="val_loss")
+  plt.plot(H["train_acc"], label="train_acc")
+  plt.plot(H["val_acc"], label="val_acc")
+  plt.title("Training Loss and Accuracy on Dataset")
+  plt.xlabel("Epoch #")
+  plt.ylabel("Loss/Accuracy")
+  plt.legend(loc="lower left")
+  # set the device we will be using to test the model
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# load the KMNIST dataset and randomly grab 10 data points
+  print("[INFO] loading the KMNIST test dataset...")
+  testData = KMNIST(root="data", train=False, download=True,transform=ToTensor())
+idxs = np.random.choice(range(0, len(testData)), size=(10,))
+testData = Subset(testData, idxs)
+# initialize the test data loader
+testDataLoader = DataLoader(testData, batch_size=1)
+# load the model and set it to evaluation mode
+#model = torch.load(args["model"]).to(device)
+model.eval()
+#plt.savefig(args["plot"])
+# serialize the model to disk
+#torch.save(model, args["model"])
   # write your code here as per instructions
   # ... your code ...
   # ... your code ...
